32c6ef1dec3337e33f61250731400d814e7ea98a
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2020-09-13 20:26:33 +0800

    Updates

---
+++ precache-manifest.bbd608f9496978506eeefa70265e3a1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36112849a0c7ef14ee2830fc22b17b72",
+    "url": "/chat-firestore/index.html"
+  },
+  {
+    "revision": "338aba739234e2597610",
+    "url": "/chat-firestore/static/css/main.849ef4ff.chunk.css"
+  },
+  {
+    "revision": "ff8886d55b0aa9a44f01",
+    "url": "/chat-firestore/static/js/2.3d0787da.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/chat-firestore/static/js/2.3d0787da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "338aba739234e2597610",
+    "url": "/chat-firestore/static/js/main.0af95a40.chunk.js"
+  },
+  {
+    "revision": "04c0d3450c07d99af050",
+    "url": "/chat-firestore/static/js/runtime-main.91dff240.js"
+  }
+]);
